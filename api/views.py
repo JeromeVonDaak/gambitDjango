4d4518a3ef8d53a744ffecb8bea87afa63ec9fa9
jerome von Daak <jerome.vondaak@proton.me>
2024-05-04 19:42:22 +0200

    python3 doesnt like the datetime.datetime.now. Changed it to datetime.datetime.utcnow()

--- api/views.py
+++ api/views.py
@@ -64,8 +64,8 @@ class LoginView(APIView):
 
             payload = {
                 'id': user.id,
-                'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=60),
-                'iat': datetime.datetime.now(datetime.UTC)
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=60),
+                'iat': datetime.datetime.utcnow()
             }
             token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
 
