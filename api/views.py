ac229b115505c845418f20374e7cea83b1e70799
jerome von Daak <jerome.vondaak@proton.me>
2024-06-12 00:14:53 +0200

    fixed some bugs

--- api/views.py
+++ api/views.py
@@ -147,10 +147,12 @@ class DeleteUserFile(APIView):
             fileid = request.data['fileid']
             file = File.objects.filter(fileid=fileid)
             file = file[0]
+            print(file.userid)
+            print(userid)
             if file.userid == userid:
                 file.delete()
                 return Response("File Deleted", status=status.HTTP_200_OK)
-            return Response("The requested file is not yours!" + file, status=status.HTTP_400_BAD_REQUEST)
+            return Response("The requested file is not yours!", status=status.HTTP_400_BAD_REQUEST)
         return Response("something went wrong !", status=status.HTTP_400_BAD_REQUEST)
 
 
